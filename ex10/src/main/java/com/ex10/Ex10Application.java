a55cf63b05419c6bb8b89d46514b20113dc034fb
jmkim <sir_jm@naver.com>
2022-10-11 16:54:05 +0900

    20221011 1655

--- ex10/src/main/java/com/ex10/Ex10Application.java
+++ ex10/src/main/java/com/ex10/Ex10Application.java
@@ -2,12 +2,21 @@ package com.ex10;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
-public class Ex10Application {
+public class Ex10Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ex10Application.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {	
+		return builder.sources(Ex10Application.class);
 	}
 
 }
